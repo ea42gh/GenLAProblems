3af314d8655a2a85b0b73b866e45dccdd9a2e9c5
ea42gh <ea42_github@mail.com>
2026-09-03 10:21:27 -0400

    Rename matrix generation source unit

--- src/GenLAProblems.jl
+++ src/GenLAProblems.jl
@@ -32,7 +32,7 @@ is_none_val(x) = x === :none || x === nothing
 
 include("IntegerFamilies.jl")
 include("SymbolNames.jl")
-include("BasicMatrices.jl")
+include("MatrixGeneration.jl")
 include("GEProblems.jl")
 include("FactorizationProblems.jl")
 include("OrthogonalProblems.jl")
